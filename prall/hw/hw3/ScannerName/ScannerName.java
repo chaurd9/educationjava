d1692c5c4efe1a32db2534118c80b89fd8c31e22
chaurd9 <ChaurdtheNinth@gmail.com>
2025-07-22 23:21:40 +0500

    Домашнее задание №3

---
+++ prall/hw/hw3/ScannerName/ScannerName.java
@@ -0,0 +1,12 @@
+package ScannerName;
+
+import java.util.Scanner;
+
+public class ScannerName {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите Имя пользователя: ");
+        String name = scanner.nextLine();
+        System.out.println("Привет, " + name);
+    }
+}
