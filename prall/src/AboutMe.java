8fb3fd7502be35ecde5a9ee446ad50642618c18c
chaurd9 <ChaurdtheNinth@gmail.com>
2025-08-17 00:11:28 +0500

    Создана структура проекта homework07 и файл .gitignore

--- prall/src/AboutMe.java
+++
@@ -1,7 +0,0 @@
-public class AboutMe {
-    public static void main(String[] args) {
-        System.out.println("Я решил попробовать для себя впервые Java, " +
-                "чтобы побаловаться и поковыряться в модах для игр, " +
-                "а также копанию в коде программ на Андроиде.");
-    }
-}
